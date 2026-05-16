8117f95f345375075bfa1814d362f055fe5bcd2d
dayanand divatagi <sangameshjadar1@gmail.com>
2026-05-16 15:06:41 +0530

    fix static root location

--- smartgarbage/settings.py
+++ smartgarbage/settings.py
@@ -94,6 +94,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- smartgarbage/urls.py
+++ smartgarbage/urls.py
@@ -20,4 +20,3 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
